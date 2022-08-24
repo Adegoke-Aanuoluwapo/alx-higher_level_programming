e4e4dbd905720e8cc3d286614052f292dc46f69c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-08-24 16:36:08 -0700

    Task 1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-string = input("Enter an Alphabet ")
-if string == 'a' or 'z' and not(q,e):
+input("Enter an Alphabet ")
+if  == 'a' or 'z' and not(q,e):
     print("The alphabet is lower")
