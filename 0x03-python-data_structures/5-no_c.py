d79ed13411d13cd691268b80997923d20e2df8a4
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-08-29 23:54:28 -0700

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    update_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            update_str += 1
+        return (update_str)
+        
