058e48b9995e01c51b021f9fa23bd7b7fee6006e
Adegokr-Aanuoluwapo <Aanummaculate@gmail.com>
2022-09-20 00:28:20 -0700

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangule:
+    """Ths represents a rectangle"""
+    def __init__(self, width =0, height = 0):
+        """Initializating this rectangle class
+        Args:
+            width: represent the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+            """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("widht must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    @property
+    def height(self):
+        """retrieves height attribute"""
+        return self.__height
+
+    @property
+    height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.height= value
+
+
+
+
+
+
